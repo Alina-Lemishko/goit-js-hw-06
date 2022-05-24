5813aead10bcd35bb4a7bcefaf550eda2ab2227e
Alina Lemishko <aliska.nofear@gmail.com>
2022-05-24 15:11:14 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  buttonEl: document.querySelector('.change-color'),
+  textEl: document.querySelector('.color'),
+}
+
+refs.buttonEl.addEventListener('click', event => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.textEl.textContent = getRandomHexColor();
+});
